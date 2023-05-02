393d3224e27b991c9d5188d04a8745b97de2cb6e
aleks_pv <apanov08@gmail.com>
2023-05-02 20:37:17 +0400

    refactor: change info

--- src/layout/info.js
+++ src/layout/info.js
@@ -3,6 +3,6 @@ import { Control } from '../common/control.js';
 export class Info extends Control {
   constructor(parentNode, tagName, className) {
     super(parentNode, tagName, className);
-    this.node.innerText = "Клавиатура выполнена на ОС Windows. Для смены языка нажмите 'Shift' + 'Alt'";    
+    this.node.innerText = "Клавиатура выполнена на ОС Windows. Для смены языка нажмите 'Shift' + 'AltLeft'";    
   }  
 }
